bfce3bec5d4e87fffc0958b061923332eed660ef
PeterNedergaard <78910318+PeterNedergaard@users.noreply.github.com>
2024-06-07 16:36:27 +0200

    Simple page for manual traffic sign prediction

--- streamlit_app/pages/chatbot.py
+++ streamlit_app/pages/chatbot.py
@@ -7,11 +7,13 @@ from dotenv import load_dotenv
 import pandas as pd
 import os
 import sys
-sys.path.append(os.path.join(os.path.dirname(__file__),'../../'))
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 import llm.llm as llm
 
 st.set_page_config(page_title="Chatbot", page_icon="../img/logo.png")
 llm = llm.LLM()
+
 
 # Streamed response emulator
 def response_generator(question):
@@ -21,9 +23,11 @@ def response_generator(question):
         yield word + " "
         time.sleep(0.10)
 
+
 def clear_chat():
     st.session_state.messages = []
     st.rerun()
+
 
 st.markdown(
     """
@@ -54,8 +58,7 @@ st.markdown(
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-
-messages = st.session_state.messages # Current chat messages
+messages = st.session_state.messages  # Current chat messages
 # Save and clear buttons
 col1, col2 = st.columns([9, 1])
 
@@ -65,19 +68,19 @@ with col2:
 # Display chat messages
 container = st.container(height=645, border=False)
 with container:
-# First message from chatbot
+    # First message from chatbot
     first_message = "Hello! How can I help you today?"
     if "assistant" not in [message["role"] for message in messages]:
         messages.append({"role": "assistant", "content": first_message})
-            
+
     # Display chat messages
     for message in messages:
         with container.chat_message(message["role"]):
             st.markdown(message["content"])
 
 # Chat message input
-if prompt := st.chat_input("Type your message",key="new_chat"):
-# Save user message to session state
+if prompt := st.chat_input("Type your message", key="new_chat"):
+    # Save user message to session state
     messages.append({"role": "user", "content": prompt})
     with container.chat_message("user"):
         st.markdown(prompt)
---
+++ streamlit_app/pages/trafficsign.py
@@ -0,0 +1,68 @@
+import os
+import streamlit as st
+from PIL import Image
+from ultralytics import YOLO
+
+st.title("Traffic Sign Recognition")
+model = YOLO("traffic_sign_model.pt")
+
+# Dummy function to simulate model prediction
+def predict(input_image):
+    prediction = model(input_image)
+    prediction[0].save("prediction.jpg")
+    output_img = Image.open("prediction.jpg").convert('RGB')
+    os.remove("prediction.jpg")
+    return output_img
+
+# Specify the directory containing the pre-defined images
+image_dir = '../img/traffic_images'
+
+# List all files in the directory
+files = os.listdir(image_dir)
+
+# Filter for image files (assuming JPEG and PNG) and open them
+images = [Image.open(os.path.join(image_dir, file)) for file in files if file.endswith(('.png', '.jpg', '.jpeg'))]
+
+css = '''
+<style>
+    [data-testid='stFileUploader'] {
+        width: max-content;
+    }
+    [data-testid='stFileUploader'] section {
+        padding: 0;
+        float: left;
+    }
+    [data-testid='stFileUploader'] section > input + div {
+        display: none;
+    }
+    [data-testid='stFileUploader'] section + div {
+        float: right;
+        padding-top: 0;
+    }
+</style>
+'''
+
+st.markdown(css, unsafe_allow_html=True)
+
+if 'selected_image' not in st.session_state:
+    st.session_state.selected_image = None
+
+cols = st.columns(len(images) + 1)  # Create columns for images + one for upload
+
+for i, img in enumerate(images):
+    with cols[i]:
+        st.image(img, use_column_width=True, output_format='PNG', caption=f"Sample {i + 1}")
+        if st.button(f"Select sample {i + 1}", key=f"select_{i}"):
+            st.session_state.selected_image = img
+
+# File uploader in the last column
+with cols[-1]:
+    uploaded_file = st.file_uploader("", type=['jpg', 'jpeg', 'png'], key="uploader")
+
+if st.session_state.selected_image or uploaded_file:
+    display_image = st.session_state.selected_image if st.session_state.selected_image else Image.open(
+        uploaded_file).convert('RGB')
+
+    # Run the model prediction
+    output_image = predict(display_image)
+    st.image(output_image, caption='Predicted Image', use_column_width=True)
