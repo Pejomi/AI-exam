ec3966f84005d6ed9a2c971499c4ff7da1bad8ec
PeterNedergaard <peter@nedergaard.org>
2024-06-09 03:26:05 +0200

    put train aov model in method

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from prediction.supervised import train
 from prediction.supervised import wrangle_data
 
 if __name__ == '__main__':
-    data = pd.read_csv('data/merged_information_clean.csv', encoding='utf-8', nrows=200000)
+    data = pd.read_csv('data/merged_information_clean.csv', encoding='utf-8')
     wrangle_data.wrangle_data(data)
 
     # cleaned_data = pd.read_csv('data/cleaned_data.csv', encoding='utf-8')
